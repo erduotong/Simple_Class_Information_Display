a6931b7bc797e426fd5f6fb62472bd093490ae04
DESKTOP-UL157US\13568 <3135351980@qq.com>
2023-08-16 23:53:13 +0800

    写了一点下载文件

--- main/updater.py
+++ main/updater.py
@@ -6,17 +6,22 @@
 import enum
 import json
 import requests
+import os
+from concurrent.futures import ThreadPoolExecutor
 
 
 class VersionStatus(enum.IntEnum):
-    # 无须更新
-    UpToDate = 0
-    # 有新版本
-    Lower = 1
-    # 无下载链接
-    NoLink = 2
-    # error, with a str error message
-    Error = 3
+    UpToDate = 0  # 无须更新
+    Lower = 1  # 有新版本
+    NoLink = 2  # 无下载链接
+    Error = 3  # error
+
+
+class DownloadStatus(enum.IntEnum):
+    Success = 0  # 下载成功
+    ErrorGetSize = 1  # 获取文件大小时出错
+    ErrorDownload = 2  # 下载时出错
+    ErrorWriteChunk = 3  # 写入文件块时出错
 
 
 # 思路            (这里要判断网络是否连接 没连接就不试了
@@ -32,7 +37,7 @@ class ProgramUpdater(object):
         self.change_log = ""
         self.download_url = ""
 
-    def get_latest_version(self, mode: str, api_link: str) -> tuple[VersionStatus, str | None]:
+    def get_latest_version(self, mode: str, api_link: str) -> VersionStatus:
         """
         获得最新的版本号,如果有就写入self中
         :param mode: 从哪个网站获取? 目前支持解析 github gitee 处获得的
@@ -41,27 +46,37 @@ class ProgramUpdater(object):
         """
         # 从api link获得后再去匹配mode
         response = requests.get(api_link)  # 获得api数据
-        
         if response.status_code != 200:  # 获取了不正常的数据
-            return (VersionStatus.Error, f"错误的状态码: {response.status_code}")
-        
+            return VersionStatus.Error
         response = json.loads(response.json())  # 得到相应的数据
         if mode in ('github', 'gitee'):
-            
             if response.get("name") == self.now_version:  # 版本相等的情况
-                return (VersionStatus.UpToDate, None)
-            
+                return VersionStatus.UpToDate
             self.new_version = response.get("name")
             self.change_log = response.get("body")
-            
             # 遍历assets以获得匹配版本类型的download_url
             assets = response.get("assets")
-            
             if any(i.get("name") == self.version_type for i in assets):
-                
                 self.download_url = response.get("browser_download_url")
-                return (VersionStatus.Lower, None)
-            
-            return (VersionStatus.NoLink, None)
-        
-        return (VersionStatus.Error, f"未知错误 {response=} {mode=} {api_link=}")
+                return VersionStatus.Lower
+            return VersionStatus.NoLink
+        return VersionStatus.Error
+
+    def download_new_version(self, destination) -> DownloadStatus:
+        """
+        下载新版本并在完成时改名为complete
+        :param destination:下载数据保存文件的路径(包括后缀名)
+        :return:状态
+        """
+        # noinspection PyBroadException
+        try:  # 获取文件大小
+            response = requests.get(self.download_url, stream=True)
+            file_size = int(response.headers.get('Content-Length', 0))  # 获得文件大小
+        except:  # 获取的时候出现错误了
+            return DownloadStatus.ErrorGetSize
+
+        # noinspection PyBroadException
+        try:  # 下载文件!
+            pass #todo
+        except:  # 没下成功
+            return DownloadStatus.ErrorDownload
